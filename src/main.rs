2d73305efa236d0731d07a73d5125c5b7cd7f34b
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2024-06-29 14:05:02 +0530

    example of "unit" literal value in rust

--- src/main.rs
+++ src/main.rs
@@ -13,4 +13,14 @@ fn main() {
     let x = x / 2;
     println!("The answer is {}", x);
     //let n :i8,i16,i32,i64,i128, isize, usize, u8,u16,u32,u64,u128
+    let unit_literal: () = ();
+    println!("{:?}", unit_literal); //prints () on console
+    let println_result = println!("Generates unit literal as a reuslt");
+    println!("{:?}", println_result); //prints () on console
+                                      /*
+                                      println!("{}", println_result);//generates the following error
+                                      the trait `std::fmt::Display` is not implemented for `()`
+                                      in format strings you may be able to use `{:?}` (or {:#?} for
+                                      pretty-print) instead
+                                      */
 }
