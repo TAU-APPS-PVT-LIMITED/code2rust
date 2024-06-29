1dd19cd467526e48b9621b1cb60690e4a4d8eff1
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2024-06-29 11:12:44 +0530

    source folder hosting the source files

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+/*
+    main function
+*/
+fn main() {
+    println!("Hello, world!"); //println! is  macro
+    println!("My name is {}", "Gaurav Gupta"); //{} substitution placeholder; same lines of python
+    let name = "GGupta"; //constant syntax
+    println!("My name is {}", name);
+    //following is an example of shadowing, constant name being reused.
+    let x = 5 + 3; //data type is i32
+    let x = x * 2;
+    let x = x - 6;
+    let x = x / 2;
+    println!("The answer is {}", x);
+    //let n :i8,i16,i32,i64,i128, isize, usize, u8,u16,u32,u64,u128
+}
